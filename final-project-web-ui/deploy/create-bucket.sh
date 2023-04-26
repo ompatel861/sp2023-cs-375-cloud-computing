c5c007285da558370d1816700ecd563b8e9643fd
ompatel861 <Ohm.Patel2@quinnipiac.edu>
2023-04-25 20:40:56 -0400

    Final Changes

--- final-project-web-ui/deploy/create-bucket.sh
+++ final-project-web-ui/deploy/create-bucket.sh
@@ -1,4 +1,4 @@
-aws s3api create-bucket --bucket sample-bucket-dn --acl public-read
+aws s3api create-bucket --bucket sample-bucket-op --acl public-read
 aws s3 website s3://sample-bucket-dn --index-document index.html
 
 # Download the zip file from S3, note the use of the S3 URI, not HTTPS
@@ -8,7 +8,7 @@ aws s3 cp s3://sample-bucket-dn/build.zip build.zip
 unzip -d web_ui_deploy build.zip
 
 # Sync up the contents of the temp directory to S3 prefix
-aws s3 sync web_ui_deploy s3://sample-bucket-dn
+aws s3 sync web_ui_deploy/build s3://sample-bucket-op
 
 # And optionally, clean up the temp files and directory
 # Unix:
